cea5ef30eb056dadd7025cfce748f75ed0790f1f
RaafinHaswoto <raafi.haswoto@gmail.com>
2018-10-04 20:12:29 +0700

    build react app

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                cell 1
+              </td>
+              <td>
+                cell 2
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+      </div>
+    );
+  }
+}
+
+export default App;
